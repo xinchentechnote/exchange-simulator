083667375689fa8d7f2d62654ccd40b576a1b854
xinchen <xinchentechnote@163.com>
2026-04-13 23:27:51 +0800

    feat: add configuration properties for SseBinServer and update handler to decode messages

--- exchange-gateway/src/main/java/com/xinchentechnote/exchange/gateway/sse/SseBinServerConfig.java
+++ exchange-gateway/src/main/java/com/xinchentechnote/exchange/gateway/sse/SseBinServerConfig.java
@@ -1,14 +1,20 @@
 package com.xinchentechnote.exchange.gateway.sse;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "sse.bin.server")
 public class SseBinServerConfig {
+
+    private int port = 9010;
 
     @Bean
     public SseBinServer sseBinServer() {
-        SseBinServer sseBinServer = new SseBinServer(9010);
+        SseBinServer sseBinServer = new SseBinServer(this.port);
         sseBinServer.start();
         return sseBinServer;
     }
