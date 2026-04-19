351f78200a9e42f39e1c367588a0573ca351e993
xinchen <xinchentechnote@163.com>
2026-04-19 23:33:21 +0800

    feat: add connection logging for active and inactive channels in SSE server handler

--- exchange-gateway/src/main/java/com/xinchentechnote/exchange/gateway/sse/SseBinServerHandler.java
+++ exchange-gateway/src/main/java/com/xinchentechnote/exchange/gateway/sse/SseBinServerHandler.java
@@ -5,12 +5,26 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.sql.SQLOutput;
+
 public class SseBinServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
     private final SseBinServer sseBinServer;
 
     public SseBinServerHandler(SseBinServer sseBinServer) {
         this.sseBinServer = sseBinServer;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("New connection from " + ctx.channel().remoteAddress());
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Connection closed from " + ctx.channel().remoteAddress());
+        super.channelInactive(ctx);
     }
 
     @Override
